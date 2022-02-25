cc80006eca58b123f1327c6215195886e8dcac0b
mpn <mpninfomail@gmail.com>
2022-02-25 12:32:47 +0330

    add attachment

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 from http import server
 import smtplib
 from email.message import EmailMessage
-
+'''
+    sending email using python
+'''
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'mpninfomail@gmail.com'
+EMAIL_HOST_USER = 'YorEmailAddress@gmail.com'
 EMAIL_PORT_SSL = 465
-EMAIL_HOST_PASSWORD=''
+EMAIL_HOST_PASSWORD='replace with your app password' """ Example : clluqiscfeduglpq"""
 
 msg = EmailMessage()
-msg['Subject']='Python email test'
+msg['Subject']='Python Test'
 msg['From']=EMAIL_HOST_USER
-msg['To']='pakniat.mahmoud@gmail.com'
-msg.set_content('This is a message body')
+msg['To']='mpn_info@yahoo.com'
+msg.set_content('This is a message body for test!')
 
-with open('log.png', 'rb') as f:
+with open('AttachmentFileName', 'rb') as f:
     file_data = f.read()
     file_name = f.name
+    
+''' change maintype and subtype for other attachment file except jpg '''
+msg.add_attachment(file_data, maintype='image', subtype='jpg', filename = file_name)
 
-msg.add_attachment(file_data, maintype='image', subtype='png', filename = file_name)
-
-with smtplib.SMTP_SSL(EMAIL_HOST, EMAIL_PORT_SSL) as Server:
-    server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+with smtplib.SMTP_SSL(EMAIL_HOST, EMAIL_PORT_SSL) as server:
+    server.login (EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
     server.send_message(msg)
+    print('your email has been sent')
